a5eee18725d629b1c6153ef2855bc628d21a5c18
Ajay Gangwar <ajayapril1991@email.com>
2026-07-01 12:59:16 +0530

    Added Query - 1st Highest Salary

---
+++ SQLDB/Find-Highest-Salary/1st-Highest-Salary.sql
@@ -0,0 +1,15 @@
+-- Select TOP 10 * from Employee Order By Salary DESC;
+
+-- SELECT * 
+-- FROM Employee
+-- ORDER BY EmployeeID
+-- OFFSET 3 ROWS
+-- FETCH NEXT 5 ROWS ONLY;
+
+-- Maximum Salary | 1st Highest Salary
+Select Max(Salary) from Employee;
+
+Select Salary From Employee 
+Order By Salary DESC
+Offset 0 Rows 
+FETCH NEXT 1 ROWS ONLY;
